c5b03cc5a68320cf1cb9dc4f5485c7d3d6d2aa4d
howard <howard.yang@kiwigame.com>
2024-11-27 20:08:54 -0600

    modify Makefile and slurm script

--- code/slurm.sh
+++ code/slurm.sh
@@ -4,4 +4,15 @@
 #SBATCH -o Task.out -e Task.err
 #SBATCH --cpus-per-task=20
 
-./open_aes
+PROGRAM=$1
+
+if [ "$PROGRAM" == "aes" ]; then
+    echo "Running aes program"
+    ./aes
+elif [ "$PROGRAM" == "open" ]; then
+    echo "Running open program"
+    ./open
+else
+    echo "Unknown program specified: $PROGRAM"
+    exit 1
+fi
