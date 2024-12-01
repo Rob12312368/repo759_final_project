07643d547c63d27257fcc7d141b1b8c6792efaa1
Rob12312368 <rob12312368@gmail.com>
2024-11-30 22:19:36 -0600

    change openmp header file

--- code_cpp/AES_OpenMp/AES.h
+++ code_cpp/AES_OpenMp/AES.h
@@ -13,9 +13,13 @@ class AES{
         void KeyExpansionCore(unsigned char* in , unsigned char i);
         void KeyExpansion(unsigned char* inputKey, unsigned char* expandedKeys);
         void SubBytes(unsigned char* state);
+        void SubBytes_Inv(unsigned char* state);
         void ShiftRows(unsigned char* state);
+        void ShiftRows_Inv(unsigned char* state)
         void MixColumns(unsigned char* state);
+        void MixColumns_Inv(unsigned char* state);
         void AddRoundKey(unsigned char* state, unsigned char* roundKey);
         void Encrypt(unsigned char* message, unsigned char* key);
+        void Decrypt(unsigned char* message, unsigned char* key);
 };
 #endif
